62d300031cdeed5b0136c03eb8d08125a7a39c09
Neeth Kunnath <neeth.xavier@gmail.com>
2024-02-08 21:42:25 -0500

    update logging.json

--- object_tracking/utils/logutils.py
+++ object_tracking/utils/logutils.py
@@ -4,17 +4,14 @@ import click
 import logging
 import logging.config
 import numpy as np
+from pathlib import Path
 import matplotlib.pyplot as plt
 
 from copy import deepcopy
 
+from object_tracking import ROOT_DIR, LOG_CFG, LOG_DIR
 
-DEBUG = 0
-INFO = 1
-WARN = 2
-ERROR = 3
-
-VERBOSE = INFO
+logger = logging.getLogger(__name__)
 
 
 def makeDictJsonReady(dictData: dict):
@@ -45,29 +42,26 @@ def prettyDumpDict(dictData):
 
 
 def error(message, verboseLvl=3):
-    secho(message, fg="red", verboseLvl=verboseLvl)
+    secho(message, fg="red")
     logging.error(message)
 
 
 def warn(message, verboseLvl=2):
-    secho(message, fg="yellow", verboseLvl=verboseLvl)
+    secho(message, fg="yellow")
     logging.warning(message)
 
 
 def info(message, verboseLvl=1):
-    secho(message, verboseLvl=verboseLvl)
+    secho(message, fg="cyan")
     logging.info(message)
 
 
 def debug(message, verboseLvl=0):
-    secho(message, fg=None, verboseLvl=verboseLvl)
+    secho(message, fg=None)
     logging.debug(message)
 
 
-def secho(message, fg="cyan", verboseLvl=0):
-    if verboseLvl < VERBOSE:
-        return  # do nothing
-
+def secho(message, fg="cyan"):
     if isinstance(message, str):
         click.secho(message, fg=fg)
     elif isinstance(message, dict):
@@ -78,21 +72,11 @@ def setupLogging(level: str = "INFO", env_key: str = "OTRK_LOG_CFG"):
     """
     Setup logging
     """
-    global VERBOSE
 
     logging.getLogger("matplotlib").setLevel(logging.WARNING)
     plt.style.use("seaborn-pastel")
 
     level = level.upper()
-
-    if level == "ERROR":
-        VERBOSE = ERROR
-    elif level == "WARN":
-        VERBOSE = WARN
-    elif level == "DEBUG":
-        VERBOSE = DEBUG
-    else:
-        VERBOSE = INFO
 
     value = os.getenv(env_key, None)
     logstr = []
@@ -110,3 +94,91 @@ def setupLogging(level: str = "INFO", env_key: str = "OTRK_LOG_CFG"):
         logstr.append(f"using basicConfig")
 
     logging.info("\n".join(logstr))
+
+
+class bcolors:
+    HEADER = "\033[95m"  # DEBUG
+    WARNING = "\033[94m"  # WARNING
+    OKCYAN = "\033[96m"  # INFO
+    OKGREEN = "\033[92m"
+    OKYELLOW = "\033[93m"
+    FAIL = "\033[91m"  # ERROR/CRITICAL
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+def set_color_value(levelname, text=None):
+    text = text or levelname
+    log_set = {
+        "DEBUG": f"{bcolors.OKYELLOW}{text}{bcolors.ENDC}",
+        "INFO": f"{bcolors.OKCYAN}{text}{bcolors.ENDC}",
+        "WARNING": f"{bcolors.WARNING}{text}{bcolors.ENDC}",
+        "ERROR": f"{bcolors.FAIL}{text}{bcolors.ENDC}",
+        "CRITICAL": f"{bcolors.FAIL}{text}{bcolors.ENDC}",
+        "HEADER": f"{bcolors.HEADER}{text}{bcolors.ENDC}",
+    }
+
+    return log_set[levelname]
+
+
+def setupLogging(
+    console_level: str = "INFO",
+    root_level="INFO",
+    log_cfg: str = "",
+    log_dir: str = "",
+):
+    """
+    Setup logging
+    """
+
+    # prevent matplotlib logs from flooding the logs
+    if logging.getLogger("matplotlib").level < logging.WARNING:
+        logging.getLogger("matplotlib").setLevel(logging.WARNING)
+
+    plt.style.use("seaborn-pastel")
+
+    logstr = []
+    try:
+        if not log_cfg:
+            log_cfg = os.path.join(ROOT_DIR, LOG_CFG)
+        logstr.append(f"logging config: {log_cfg}")
+
+        if not log_dir:
+            log_dir = os.path.join(ROOT_DIR, LOG_DIR)
+        logstr.append(f"logs to be written to {log_dir}")
+        console_level = console_level.upper()
+        root_level = root_level.upper()
+
+        if Path(log_cfg).exists():
+            if not Path(log_dir).exists():
+                os.makedirs(log_dir)
+            with open(log_cfg, "rt") as f:
+                config = json.load(f)
+                config["root"]["level"] = root_level
+                logstr.append(f"root log level: {root_level}")
+
+                # extra config for console formatter
+                config["handlers"]["console"]["level"] = console_level
+                c_fmt = config["formatters"]["console"]["format"]
+                c_fmt = c_fmt.replace("%(levelname)s", set_color_value(console_level, "%(levelname)s"))
+                c_fmt = c_fmt.replace("%(name)s", set_color_value("HEADER", "%(name)s"))
+                config["formatters"]["console"]["format"] = c_fmt
+                logstr.append(f"console log level: {console_level}")
+
+                # set log dir
+                handlers = config.get("handlers", {})
+                for h_name, h in handlers.items():
+                    file_name = h.get("filename", "")
+                    if file_name:
+                        h["filename"] = str(Path(log_dir) / file_name)
+                    handlers[h_name] = h
+                config["handlers"] = handlers
+            logging.config.dictConfig(config)
+            logstr.append(f"logging init from provided config: {log_cfg}")
+        else:
+            logging.basicConfig(level=console_level)
+            logstr.append(f"using basicConfig with log level: {console_level}")
+    finally:
+        for msg in logstr:
+            logger.debug(msg)
